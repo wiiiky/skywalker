8a21d76b0ae822de3b93b599fbb8aa66098dbf30
wiiiky <wiiiky@outlook.com>
2016-03-08 20:03:50 +0800

    Direct协议

---
+++ src/skywalker/agent/direct/direct.go
@@ -0,0 +1,52 @@
+/*
+ * Copyright (C) 2015 Wiky L
+ *
+ * This program is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.";
+ */
+
+package direct
+
+type DirectAgent struct {
+}
+
+func NewDirectAgent() *DirectAgent {
+    return &DirectAgent{}
+}
+
+func (a *DirectAgent) Name() string {
+    return "Direct"
+}
+
+func (a *DirectAgent) OnStart(cfg map[string]interface{}) error {
+    return nil
+}
+
+func (a *DirectAgent) GetRemoteAddress(addr string, port string) (string, string){
+    return addr, port
+}
+
+func (a *DirectAgent) OnConnected() (interface{}, interface{}, error){
+    return nil, nil, nil
+}
+
+func (a *DirectAgent) FromServer(data []byte) (interface{}, interface{}, error){
+    return data, nil, nil
+}
+
+func (a *DirectAgent) FromClientAgent(data []byte) (interface{}, interface{}, error) {
+    return data, nil, nil
+}
+
+func (a *DirectAgent) OnClose() {
+}
