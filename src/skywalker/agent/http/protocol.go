13fd5ea32773beca14ccfede0731c686a48781c2
wiiiky <wiiiky@outlook.com>
2016-03-29 12:09:34 +0800

    parse

--- src/skywalker/agent/http/protocol.go
+++ src/skywalker/agent/http/protocol.go
@@ -17,17 +17,27 @@
 
 package http
 
+import (
+    "bytes"
+    "skywalker/agent"
+)
+
 
 /*
  * HTTP代理协议
  */
 
 const (
-    http_METHOD_GET = "GET"
-    http_METHOD_POST = "POST"
-    http_METHOD_PUT = "PUT"
-    http_METHOD_DELETE = "DELETE"
-    http_METHOD_CONNECT = "CONNECT"
+    HTTP_METHOD_GET = "GET"
+    HTTP_METHOD_POST = "POST"
+    HTTP_METHOD_PUT = "PUT"
+    HTTP_METHOD_DELETE = "DELETE"
+    HTTP_METHOD_CONNECT = "CONNECT"
+)
+
+const (
+    ERROR_INVALID_FORMAT = 1
+    ERROR_INVALID_METHOD = 2
 )
 
 
@@ -35,11 +45,31 @@ type httpRequest struct {
     method string
     host string
     path string
+    version string
     content_length int
     headers map[string]string
     body []byte
 
     data []byte
+}
+
+func (req *httpRequest) parse(data []byte) (bool, error) {
+    elements := bytes.Split(req.data, []byte{'\n'})
+    if len(elements) <= 1 {
+        return false, nil
+    }
+    first := bytes.Split(elements[0], []byte{' '})
+    if len(first) != 3{ /* 第一行少于三个元素 */
+        return false, agent.NewAgentError(ERROR_INVALID_FORMAT, "invalid format")
+    }
+    method := string(first[0])
+    if !(method == HTTP_METHOD_GET || method == HTTP_METHOD_POST ||
+        method == HTTP_METHOD_PUT || method == HTTP_METHOD_DELETE ||
+        method == HTTP_METHOD_CONNECT) {
+        return false, agent.NewAgentError(ERROR_INVALID_METHOD, "invalid method %s", first[0])
+    }
+
+    return false, nil
 }
 
 /*
@@ -49,5 +79,5 @@ type httpRequest struct {
  */
 func (req *httpRequest) feed(data []byte) (bool, error) {
     req.data = append(req.data, data...)
-    return false, nil
+    return req.parse(req.data)
 }
